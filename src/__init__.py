07f6fd60023d25ea11ed0ad676f6b19784623a4f
Tanaya Hingwe <tanayahingwe1212@gmail.com>
2025-11-01 23:03:16 +0530

    setup

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+#the find_packeages() of setup.py will search for __init__.py as it is a package and will try to build it and later importing  
+
+#when python sees a folder with an __init__.py file, it treats that folder as a package that can be imported.
+
